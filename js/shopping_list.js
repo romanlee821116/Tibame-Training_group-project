38e4541ac5ad5a2e280e8eb0d62c6ecda76ba2d3
a88376219 <a88376219@gmail.com>
2021-07-23 01:31:09 +0800

    no message

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -1,57 +1,56 @@
 $(document).ready(function() {
     $('.product_product').first().show().siblings().hide();
-    // $('.product_area').first().find("i").addClass("yellow").show();
-    // $('.product_area').first().find('span').css({ color: '#172852' });
-    // $('.product_area').first().find("img").attr('src', "../images/shop1_hov.png");
-    // 點擊切換商品分類內容
+    $('.product_area').first().find("i").addClass("yellow").show();
+    $('.product_area').first().find('span').css({ color: '#172852' });
+    $('.product_area').first().find("img").attr('src', "../images/shopping_list/shop1_hov.png");
+    // 選擇商品分類
+    $(".product_area").mouseenter(function() {
+            $(this).find("i").addClass("yellow").show();
+
+            if ($(this).data("num") == 1) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop1_hov.png");
+            } else if ($(this).data("num") == 2) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop2_hov.png");
+            } else if ($(this).data("num") == 3) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop3_hov.png");
+            } else if ($(this).data("num") == 4) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop4_hov.png");
+            } else if ($(this).data("num") == 5) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop5_hov.png");
+            } else if ($(this).data("num") == 6) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop6_hov.png");
+            } else if ($(this).data("num") == 7) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop7_hov.png");
+            }
+
+            $(this).find('span').css({ color: '#172852' });
+        }).mouseleave(function() {
+            $(this).find("i").addClass("yellow").hide();
+
+            if ($(this).data("num") == 1) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop1.png");
+            } else if ($(this).data("num") == 2) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop2.png");
+            } else if ($(this).data("num") == 3) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop3.png");
+            } else if ($(this).data("num") == 4) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop4.png");
+            } else if ($(this).data("num") == 5) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop5.png");
+            } else if ($(this).data("num") == 6) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop6.png");
+            } else if ($(this).data("num") == 7) {
+                $(this).find("img").attr('src', "../images/shopping_list/shop7.png");
+            }
+            $(this).find('span').css({ color: '#bb866a' });
+        })
+        // 點擊切換商品分類內容
     $(".product_area").click(function() {
         var index = $(this).index();
-        $(".product_product").eq(index).show().siblings().hide();
+        $(".product_product").eq(index).fadeIn(500).show().siblings().hide();
         $(this).find("i").addClass("yellow").show();
         $(this).find('span').css({ color: '#172852' });
     });
-    // 選擇商品分類
-    $(".product_area").mouseenter(function() {
-        $(this).find("i").addClass("yellow").show();
-
-        if ($(this).data("num") == 1) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop1_hov.png");
-        } else if ($(this).data("num") == 2) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop2_hov.png");
-        } else if ($(this).data("num") == 3) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop3_hov.png");
-        } else if ($(this).data("num") == 4) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop4_hov.png");
-        } else if ($(this).data("num") == 5) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop5_hov.png");
-        } else if ($(this).data("num") == 6) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop6_hov.png");
-        } else if ($(this).data("num") == 7) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop7_hov.png");
-        }
-
-        $(this).find('span').css({ color: '#172852' });
-    }).mouseleave(function() {
-        $(this).find("i").addClass("yellow").hide();
-
-        if ($(this).data("num") == 1) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop1.png");
-        } else if ($(this).data("num") == 2) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop2.png");
-        } else if ($(this).data("num") == 3) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop3.png");
-        } else if ($(this).data("num") == 4) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop4.png");
-        } else if ($(this).data("num") == 5) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop5.png");
-        } else if ($(this).data("num") == 6) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop6.png");
-        } else if ($(this).data("num") == 7) {
-            $(this).find("img").attr('src', "../images/shopping_list/shop7.png");
-        }
-
-        $(this).find('span').css({ color: '#bb866a' });
-    })
 
     // 商品數量增減 
 
@@ -94,6 +93,15 @@ $(document).ready(function() {
 
     });
     // 照片slider
-
+    let index = 0;
+    let divWidth = $('.product_left').width();
+    $(".product_circle div").click(function() {
+        index = ($(this).index());
+        $(this).parent().prev('.product_img').animate({
+            left: divWidth * index * -1
+        });
+        $(this).addClass("click");
+        $(".product_circle div").not(this).removeClass("click");
+    })
 
 });
