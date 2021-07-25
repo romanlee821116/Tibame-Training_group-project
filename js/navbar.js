c453a041f16222bf3208fa3b6d79ab67aa0eb5f9
chaoowen <chanffe@gmail.com>
2021-07-25 10:59:59 +0800

    登入註冊 小更新

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,24 @@
-$(function() {
+$(document).ready(function() {
     // ------引入登入註冊頁面------
     // 載入會員頁面
     $('.memberShow').load('login.html').hide();
+    // $('.memberShow').load('member.html').hide();
+
     // 點擊打開
     $('.navbar-icon>a:first').click(function(){
+        // 隱藏所有頁面
         $('.memberAll').hide();
         $('.footer').hide();
-        $('.navbar').hide();
-        $('.memberShow').load('login.html').fadeIn(700);
+        $('.navbar').hide();    
+        
+        // 開啟登入註冊or會員中心
+        // if($('#loginFormPage .loginButton').hasClass('loginOK')){
+        //     $('.memberShow').load('member.html').fadeIn(700);
+        // }else{
+            $('.memberShow').load('login.html').fadeIn(700);
+        // };
+        
+        
     });
     // 點擊關閉
     $('body').on('click','.loginClose',function(){
