4020ff0819e37c6bf8dcc799f503dcac477b2faf
a88376219 <a88376219@gmail.com>
2021-07-28 04:05:04 +0800

    no message

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // $('.memberShow').load('login.html').hide();
+    $('.memberShow').load('login.html').hide();
     // 儲存人頭為會員中心
     // window.onload = function (){
     //     if($('#loginFormPage .loginButton').hasClass('loginOK')){
@@ -7,28 +7,28 @@ $(document).ready(function() {
     //         console.log('have');
     //     }
     // };
-    
+
     // 點擊打開
-    $('.navbar-icon .fa-user').click(function(){
+    $('.navbar-icon .fa-user').click(function() {
         // ----------嘗試用load寫-----------
         // // 隱藏所有頁面
         $('.footer').hide();
-        $('.navbar').hide();    
-        
+        $('.navbar').hide();
+
         // // 開啟登入註冊or會員中心
-        if($('#loginFormPage .loginButton').hasClass('loginOK') || $('.navbar-icon>a:first').hasClass('loginOK')){
+        if ($('#loginFormPage .loginButton').hasClass('loginOK') || $('.navbar-icon>a:first').hasClass('loginOK')) {
             window.location.href = 'member.html';
             // console.log('haveclass');
-        }else{
+        } else {
             // $('.memberShow').fadeIn(700);
             $('.memberShow').load('login.html').fadeIn(700);
             $('body').addClass('stopScroll');
             // console.log('noclass');
         };
         // ---------------------------------
-        
-        
-        
+
+
+
         // 全部用跳轉頁面寫------------
         // if($('.navbar-icon>a:first').hasClass('loginOK')){
         //     window.location.href = 'member.html';
@@ -36,11 +36,11 @@ $(document).ready(function() {
         //     window.location.href = 'login.html';
         // };
 
-        
-        
+
+
     });
     // 點擊關閉
-    $('body').on('click','.loginClose',function(){
+    $('body').on('click', '.loginClose', function() {
         $('.memberShow').fadeOut(500);
         $('.footer').show();
         $('.navbar').show();
