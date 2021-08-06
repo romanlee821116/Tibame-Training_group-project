778252bd9d7edd1a04c7969999409de7613b8a77
a88376219 <a88376219@gmail.com>
2021-08-06 11:34:12 +0800

    優化愛心功能

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -128,14 +128,14 @@ $(document).ready(function() {
     $('.product_heart').click(function() {
         $(this).toggleClass('product_heart_red');
         if ($(this).hasClass("product_heart_red")) {
-            $('.product_reminder').text("成功加入收藏").fadeIn();
+            $('.product_reminder2').fadeIn();
             setTimeout(function() {
-                $('.product_reminder').fadeOut();
+                $('.product_reminder2').fadeOut();
             }, 1000);
         } else {
-            $('.product_reminder').text("成功取消收藏").fadeIn();
+            $('.product_reminder3').fadeIn();
             setTimeout(function() {
-                $('.product_reminder').fadeOut();
+                $('.product_reminder3').fadeOut();
             }, 1000);
         }
     });
@@ -162,4 +162,55 @@ $(document).ready(function() {
     } else {
         $('.nav_logOut').fadeOut();
     }
+
+
+    // =========================== php function =========================================
+
+
+    //當頁面剛載入的時候，我就要傳送一個ajax請求，向後臺詢問“我收藏了哪些文章”
+    $.post('./favor_str.php', null, function(res) {
+        //根據返回的字串，進行收藏div的渲染
+        let titles = $('#article').children('a');
+        let arr = res.split(';');
+        arr.pop(); //因為用資料庫用分號隔開的，所以最後會多個分號，轉換的陣列最後一項是空的，要把最後的空字串去掉
+        let inner = '';
+        for (let i = 0; i < arr.length; i) {
+            inner = '<li>'
+            arr[i]
+            '</li>';
+        }
+        $('#list').html(inner);
+        //迴圈所有的按鈕
+        for (let i = 0; i < titles.length; i) {
+            let $titleText = $(titles[i]).text();
+            let $btn = $(titles[i]).next();
+            //如果我所點的這個按鈕對應的文章標題在ajax收到的結果字串裡是存在的，就顯示“已收藏”
+            if (res.indexOf($titleText) != -1) {
+                $btn.val('已收藏');
+            } else { //否則顯示“收藏”
+                $btn.val('收藏');
+            }
+            //為每個按鈕繫結點選事件                
+            $btn.click(function() {
+                if ($btn.val() == '已收藏') {
+                    //如果已收藏，則彈框並終止點選事件函式
+                    alert('你已收藏過！')
+                    return;
+                }
+                //走到這裡說明沒有收藏過，那我就要傳送ajax請求，在user_favorite欄位裡儲存title
+                $.post('./favorite.php', {
+                    'title': $titleText
+                }, function(res) {
+                    //根據返回的結果，即時地渲染進收藏的div裡，一個個append
+                    //注：這裡的res和上面的res是兩個ajax請求的結果，是完全不相同的
+                    let arr = res.split(';');
+                    arr.pop(); //把最後的空字串去掉
+                    $btn.val('已收藏');
+                    let $newLi = $('<li>');
+                    $newLi.html(arr[arr.length - 1]);
+                    $('#list').append($newLi);
+                })
+            })
+        }
+    })
 });
