9f1974b046da34c3198f72484641cdf103db84d0
a88376219 <a88376219@gmail.com>
2021-08-11 17:04:18 +0800

    no message

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+
     //========================================local storage========================================
     let cart_item = {
         'productCat': 0,
